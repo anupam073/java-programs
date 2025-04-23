1b0fcc21ff7523c0ee058c430e554ce17026ecab
anupam mohanty <anupam.2003.mohanty@gmail.com>
2025-04-23 20:10:07 +0530

    upadted files

---
+++ Integer/primeNumber.java
@@ -0,0 +1,33 @@
+package Integer;
+
+import java.util.Scanner;
+
+public class primeNumber {
+	
+	public static void main (String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number to Check: ");
+		int num = sc.nextInt();
+		boolean isprime = false;
+		
+		if (num<=1) {
+			isprime=false;
+		}
+		isprime=true;
+		for(int i =2; i<=Math.sqrt(num); i++) {
+			if(num%i ==0) {
+				isprime= false;
+				
+			}
+		}
+		
+		if(isprime) {
+			System.out.println("prime");
+		}
+		else {
+			System.out.println("not prime");
+		}
+		
+	}
+
+}
